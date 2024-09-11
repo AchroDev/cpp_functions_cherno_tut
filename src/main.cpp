2bc2c18ef4ced523009762c889c8a9e6edfe7745
AchroDev <achrodev@proton.me>
2024-09-10 19:19:24 -0500

    Added function examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+// Function with expected int return type.
+int Multiply(int a, int b)
+{
+    // Body of function returning the product of int a * int b.
+    return a * b;
+}
+
+// Void declares a function or method doesn't return anything and will log to console instead.
+// Methods are stored in a Class.
+void MultiplyAndLog(int a, int b)
+{
+    // The result integer stores the result of the Multiply function
+    int result = Multiply(a, b);
+    // Printing to the consle the stored value in the result integer.
+    std::cout << result << std::endl;
+}
+
 int main(){
-    std::string message = "Hello World";
-    std::cout << message << std::endl;
+    // Calling the MultiplyAndLog function and providing the parameters.
+    MultiplyAndLog(3, 2);
+    // Waits for the return key before exit
+    std::cin.get();
 }
